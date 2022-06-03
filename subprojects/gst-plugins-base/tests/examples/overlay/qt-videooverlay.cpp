732b90265c4957cbe4b68b9d9ac052008dc78aaf
Marc Leeman <m.leeman@televic.com>
2022-06-03 11:29:04 +0200

    gst-plugins-base: extend the example to pass mouse events from Qt to GStreamer

--- subprojects/gst-plugins-base/tests/examples/overlay/qt-videooverlay.cpp
+++ subprojects/gst-plugins-base/tests/examples/overlay/qt-videooverlay.cpp
@@ -30,6 +30,7 @@
 #include <QApplication>
 #include <QTimer>
 #include <QWidget>
+#include <QMouseEvent>
 
 /* slightly convoluted way to find a working video sink that's not a bin,
  * one could use autovideosink from gst-plugins-good instead
@@ -44,7 +45,7 @@ find_video_sink (void)
     sret = gst_element_set_state (sink, GST_STATE_READY);
     if (sret == GST_STATE_CHANGE_SUCCESS)
       return sink;
-  
+
     gst_element_set_state (sink, GST_STATE_NULL);
     gst_object_unref (sink);
   }
@@ -53,7 +54,7 @@ find_video_sink (void)
     sret = gst_element_set_state (sink, GST_STATE_READY);
     if (sret == GST_STATE_CHANGE_SUCCESS)
       return sink;
-  
+
     gst_element_set_state (sink, GST_STATE_NULL);
     gst_object_unref (sink);
   }
@@ -67,17 +68,62 @@ find_video_sink (void)
       gst_object_unref (sink);
       return NULL;
     }
-  
+
     sret = gst_element_set_state (sink, GST_STATE_READY);
     if (sret == GST_STATE_CHANGE_SUCCESS)
       return sink;
-  
+
     gst_element_set_state (sink, GST_STATE_NULL);
     gst_object_unref (sink);
   }
 
   return NULL;
 }
+
+static GstNavigation*
+get_navigation_iface(GstElement *sink)
+{
+  GstNavigation *nav = NULL;
+
+  /* The videosink should have the X overlay interface */
+  if (GST_IS_NAVIGATION (sink)) {
+    GST_DEBUG ("Found navigation: %s", GST_OBJECT_NAME (sink));
+    nav = GST_NAVIGATION (sink);
+    gst_object_ref(nav);
+  } else {
+    GST_WARNING ("No navigation found");
+  }
+
+  return nav;
+}
+
+class Window : public QWidget{
+  public:
+    GstNavigation *nav = NULL;
+
+    Window() {
+      setMouseTracking(true);
+      setMinimumSize(100, 100);
+    }
+
+    void mouseMoveEvent(QMouseEvent *ev) override {
+      if (nav) {
+        gst_navigation_send_mouse_event (nav, "mouse-move", 0, ev->pos().x(), ev->pos().y());
+      }
+    }
+
+    void mousePressEvent(QMouseEvent *ev) override {
+      if (nav) {
+        gst_navigation_send_mouse_event (nav, "mouse-button-press", ev->button(), ev->pos().x(), ev->pos().y());
+      }
+    }
+
+    void mouseReleaseEvent(QMouseEvent *ev) override {
+      if (nav) {
+        gst_navigation_send_mouse_event (nav, "mouse-button-release", ev->button(), ev->pos().x(), ev->pos().y());
+      }
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -89,21 +135,30 @@ int main(int argc, char *argv[])
 
   GstElement *pipeline = gst_pipeline_new ("xvoverlay");
   GstElement *src = gst_element_factory_make ("videotestsrc", NULL);
+  GstElement *navi = gst_element_factory_make ("navigationtest", NULL);
   GstElement *sink = find_video_sink ();
+  GstNavigation *nav = NULL;
 
   if (sink == NULL)
     g_error ("Couldn't find a working video sink.");
 
-  gst_bin_add_many (GST_BIN (pipeline), src, sink, NULL);
-  gst_element_link (src, sink);
-  
-  /* prepare the ui */
+  nav = get_navigation_iface(sink);
 
-  QWidget window;
+  if (nav == NULL)
+    g_warning("Could not find navigation interface, mouse events will not work.");
+
+  gst_bin_add_many (GST_BIN (pipeline), src, navi, sink, NULL);
+  gst_element_link_many (src, navi, sink, NULL);
+
+  /* prepare the ui
+   * Window is a QWidget where the mouse events are passed to video sink
+   */
+  Window window;
+  window.nav = nav;
   window.resize(320, 240);
   window.setWindowTitle("GstVideoOverlay Qt demo");
   window.show();
-  
+
   WId xwinid = window.winId();
   gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (sink), xwinid);
 
@@ -119,9 +174,10 @@ int main(int argc, char *argv[])
   }
 
   int ret = app.exec();
-  
+
   window.hide();
   gst_element_set_state (pipeline, GST_STATE_NULL);
+  if (nav) gst_object_unref(nav);
   gst_object_unref (pipeline);
 
   return ret;
