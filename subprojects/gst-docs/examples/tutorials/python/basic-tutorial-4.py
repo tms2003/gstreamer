9a7181dead492088bcff82c08f8b22967a898132
Filip Hanes <filip@hanes.tech>
2022-09-20 23:13:16 +0200

    update time-management.md and basic-tutorial-4.py

--- subprojects/gst-docs/examples/tutorials/python/basic-tutorial-4.py
+++ subprojects/gst-docs/examples/tutorials/python/basic-tutorial-4.py
@@ -33,19 +33,25 @@ class Tutorial4:
         # Set the URI to play
         self.playbin.set_property("uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm")
 
+        # Start playing
+        ret = self.playbin.set_state(Gst.State.PLAYING)
+        if ret == Gst.StateChangeReturn.FAILURE:
+            print("Unable to set the pipeline to the playing state.")
+            sys.exit(1)
+
         # Listen to the bus
         bus = self.playbin.get_bus()
         while not self.terminate:
             msg = bus.timed_pop_filtered(100 * Gst.MSECOND,
-                    Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR |
-                    Gst.MessageType.EOS | Gst.MessageType.DURATION_CHANGED)
+                    Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR
+                    | Gst.MessageType.EOS | Gst.MessageType.DURATION_CHANGED)
 
             # Parse message
             if msg:
                 self.handle_message(msg)
             else:
                 if self.playing:
-                    current = -1;
+                    current = -1
 
                     # Query the current position of the stream
                     ok, current = self.playbin.query_position(Gst.Format.TIME)
@@ -64,7 +70,8 @@ class Tutorial4:
                     # If seeking is enabled, we have not done it yet, and the time is right, seek
                     if self.seek_enabled and not seek_done and current > 10 * Gst.SECOND:
                         print("\nReached 10s, performing seek...")
-                        self.playbin.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, 30 * Gst.SECOND)
+                        self.playbin.seek_simple(Gst.Format.TIME,
+                            Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, 30 * Gst.SECOND)
                         seek_done = True
 
         self.playbin.set_state(Gst.State.NULL)
@@ -78,26 +85,26 @@ class Tutorial4:
         elif msg.type == Gst.MessageType.EOS:
             logger.info("End-Of-Stream reached.")
             self.terminate = True
-        elif msg.type == Gst.MessageType.DURATION:
+        elif msg.type == Gst.MessageType.DURATION_CHANGED:
             # The duration has changed, mark the current one as invalid
             self.duration = Gst.CLOCK_TIME_NONE
         elif msg.type == Gst.MessageType.STATE_CHANGED:
-          # We are only interested in state-changed messages from the pipeline
-          if msg.src == self.playbin:
-            old_state, new_state, self.pending_state = msg.parse_state_changed()
-            logger.info("Pipeline state changed from", old_state, "to", new_state)
-            # Remember whether we are in the PLAYING state or not
-            playing = new_state == Gst.State.PLAYING
-            if playing:
-                query = Gst.Query.new_seeking(Gst.Format.TIME)
-                if self.playbin.query(query):
-                    seek_enabled, start, end = query.parse_seeking()
-                    if seek_enabled:
-                        logger.info(f"Seeking is ENABLED from {start} to {end}")
+            # We are only interested in state-changed messages from the pipeline
+            if msg.src == self.playbin:
+                old_state, new_state, self.pending_state = msg.parse_state_changed()
+                logger.info("Pipeline state changed from %s to %s", old_state, new_state)
+                # Remember whether we are in the PLAYING state or not
+                playing = new_state == Gst.State.PLAYING
+                if playing:
+                    query = Gst.Query.new_seeking(Gst.Format.TIME)
+                    if self.playbin.query(query):
+                        f, seek_enabled, start, end = query.parse_seeking()
+                        if seek_enabled:
+                            logger.info(f"Seeking is ENABLED from {start} to {end}")
+                        else:
+                            logger.info("Seeking is DISABLED for this stream.")
                     else:
-                        logger.info("Seeking is DISABLED for this stream.")
-                else:
-                    logger.error("Seeking query failed.")
+                        logger.error("Seeking query failed.")
         else:
             # We should not reach here
             logger.error("Unexpected message received.")
