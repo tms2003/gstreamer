7b0a80447ac3d7a9aabf75f1fbc1499b51ab79c5
Edward Hervey <edward@centricular.com>
2023-02-09 17:48:25 +0100

    gst-play: Allow early seek with playbin3/decodebin3
    With streams-aware elements, we can seek the moment we get a stream
    collection (the provider is "ready"). Therefore send the initial seek (if
    requested) directly to that element.

--- subprojects/gst-plugins-base/tools/gst-play.c
+++ subprojects/gst-plugins-base/tools/gst-play.c
@@ -130,6 +130,8 @@ typedef struct
 static gboolean quiet = FALSE;
 static gboolean instant_rate_changes = FALSE;
 
+static GstBusSyncReply play_bus_sync_msg (GstBus * bus, GstMessage * msg,
+    gpointer user_data);
 static gboolean play_bus_msg (GstBus * bus, GstMessage * msg, gpointer data);
 static gboolean play_next (GstPlay * play);
 static gboolean play_prev (GstPlay * play);
@@ -137,6 +139,8 @@ static gboolean play_timeout (gpointer user_data);
 static void play_about_to_finish (GstElement * playbin, gpointer user_data);
 static void play_reset (GstPlay * play);
 static void play_set_relative_volume (GstPlay * play, gdouble volume_step);
+static gboolean play_do_seek_full (GstPlay * play, GstElement * target,
+    gint64 pos, gdouble rate, GstPlayTrickMode mode);
 static gboolean play_do_seek (GstPlay * play, gint64 pos, gdouble rate,
     GstPlayTrickMode mode);
 
@@ -248,6 +252,10 @@ play_new (gchar ** uris, const gchar * audio_sink, const gchar * video_sink,
 
   play->loop = g_main_loop_new (NULL, FALSE);
 
+  if (start_position > 0.0) {
+    gst_bus_set_sync_handler (GST_ELEMENT_BUS (play->playbin),
+        play_bus_sync_msg, play, NULL);
+  }
   play->bus_watch = gst_bus_add_watch (GST_ELEMENT_BUS (play->playbin),
       play_bus_msg, play);
 
@@ -365,6 +373,31 @@ play_install_missing_plugins (GstPlay * play)
   /* FIXME: implement: try to install any missing plugins we haven't
    * tried to install before */
   return FALSE;
+}
+
+static GstBusSyncReply
+play_bus_sync_msg (GstBus * bus, GstMessage * msg, gpointer user_data)
+{
+  GstPlay *play = user_data;
+
+  switch (GST_MESSAGE_TYPE (msg)) {
+    case GST_MESSAGE_STREAM_COLLECTION:
+    {
+      if (play->start_position > 0.0) {
+        gst_print (_("Early Seek to %f on %s\n"), play->start_position,
+            GST_OBJECT_NAME (GST_MESSAGE_SRC (msg)));
+        if (play_do_seek_full (play, (GstElement *) GST_MESSAGE_SRC (msg),
+                play->start_position * GST_SECOND, play->rate,
+                play->trick_mode))
+          play->start_position = 0.0;
+      }
+      break;
+    }
+    default:
+      break;
+  }
+
+  return GST_BUS_PASS;
 }
 
 static gboolean
@@ -997,7 +1030,8 @@ play_set_rate_and_trick_mode (GstPlay * play, gdouble rate,
 }
 
 static gboolean
-play_do_seek (GstPlay * play, gint64 pos, gdouble rate, GstPlayTrickMode mode)
+play_do_seek_full (GstPlay * play, GstElement * target, gint64 pos,
+    gdouble rate, GstPlayTrickMode mode)
 {
   GstSeekFlags seek_flags;
   GstQuery *query;
@@ -1005,7 +1039,7 @@ play_do_seek (GstPlay * play, gint64 pos, gdouble rate, GstPlayTrickMode mode)
   gboolean seekable = FALSE;
 
   query = gst_query_new_seeking (GST_FORMAT_TIME);
-  if (!gst_element_query (play->playbin, query)) {
+  if (!gst_element_query (target, query)) {
     gst_query_unref (query);
     return FALSE;
   }
@@ -1043,7 +1077,7 @@ play_do_seek (GstPlay * play, gint64 pos, gdouble rate, GstPlayTrickMode mode)
         seek_flags | GST_SEEK_FLAG_INSTANT_RATE_CHANGE,
         GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE,
         GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE);
-    if (gst_element_send_event (play->playbin, seek)) {
+    if (gst_element_send_event (target, seek)) {
       goto done;
     }
   }
@@ -1064,13 +1098,19 @@ play_do_seek (GstPlay * play, gint64 pos, gdouble rate, GstPlayTrickMode mode)
         /* start */ GST_SEEK_TYPE_SET, 0,
         /* stop */ GST_SEEK_TYPE_SET, pos);
 
-  if (!gst_element_send_event (play->playbin, seek))
+  if (!gst_element_send_event (target, seek))
     return FALSE;
 
 done:
   play->rate = rate;
   play->trick_mode = mode & ~GST_PLAY_TRICK_MODE_INSTANT_RATE;
   return TRUE;
+}
+
+static gboolean
+play_do_seek (GstPlay * play, gint64 pos, gdouble rate, GstPlayTrickMode mode)
+{
+  return play_do_seek_full (play, play->playbin, pos, rate, mode);
 }
 
 static void
