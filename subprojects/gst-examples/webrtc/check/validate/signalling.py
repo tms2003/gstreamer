cff32ed15ad4137911df610177d678f42958dd8d
Matthew Waters <matthew@centricular.com>
2023-05-05 17:30:46 +1000

    examples/webrtc/validate/signalling: conform to python indent style

--- subprojects/gst-examples/webrtc/check/validate/signalling.py
+++ subprojects/gst-examples/webrtc/check/validate/signalling.py
@@ -27,13 +27,15 @@ import logging
 from observer import Signal, StateObserver, WebRTCObserver, DataChannelObserver
 from enums import SignallingState, NegotiationState, DataChannelState
 
-l = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+
 
 class AsyncIOThread(threading.Thread):
     """
     Run an asyncio loop in another thread.
     """
-    def __init__ (self, loop):
+
+    def __init__(self, loop):
         threading.Thread.__init__(self)
         self.loop = loop
 
@@ -49,6 +51,7 @@ class SignallingClientThread(object):
     """
     Connect to a signalling server
     """
+
     def __init__(self, server):
         # server string to connect to.  Passed directly to websockets.connect()
         self.server = server
@@ -80,13 +83,13 @@ class SignallingClientThread(object):
 
     async def _a_loop(self):
         self._running = True
-        l.info('loop started')
+        logger.info('loop started')
         await self._a_connect()
         self.wss_connected.fire()
         assert self.conn
         async for message in self.conn:
             self.message.fire(message)
-        l.info('loop exited')
+        logger.info('loop exited')
 
     def send(self, data):
         # send some information to the peer
@@ -95,13 +98,14 @@ class SignallingClientThread(object):
         self._loop.call_soon_threadsafe(lambda: asyncio.ensure_future(_a_send()))
 
     def stop(self):
-        if self._running == False:
+        if not self._running:
             return
 
         cond = threading.Condition()
 
         # asyncio, why you so complicated to stop ?
         tasks = asyncio.all_tasks(self._loop)
+
         async def _a_stop():
             if self.conn:
                 await self.conn.close()
@@ -109,21 +113,21 @@ class SignallingClientThread(object):
 
             to_wait = [t for t in tasks if not t.done()]
             if to_wait:
-                l.info('waiting for ' + str(to_wait))
+                logger.info('waiting for ' + str(to_wait))
                 done, pending = await asyncio.wait(to_wait)
             with cond:
-                l.error('notifying cond')
+                logger.error('notifying cond')
                 cond.notify()
             self._running = False
 
         with cond:
             self._loop.call_soon_threadsafe(lambda: asyncio.ensure_future(_a_stop()))
-            l.error('cond waiting')
+            logger.error('cond waiting')
             cond.wait()
-            l.error('cond waited')
+            logger.error('cond waited')
         self._thread.stop_thread()
         self._thread.join()
-        l.error('thread joined')
+        logger.error('thread joined')
 
 
 class WebRTCSignallingClient(SignallingClientThread):
@@ -131,6 +135,7 @@ class WebRTCSignallingClient(SignallingClientThread):
     Signalling client implementation.  Deals wit session management over the
     signalling protocol.  Sends and receives from a peer.
     """
+
     def __init__(self, server, id_):
         super().__init__(server)
 
@@ -152,35 +157,35 @@ class WebRTCSignallingClient(SignallingClientThread):
         self.have_json = Signal()
 
     def _update_state(self, new_state):
-        self._state_observer.update (new_state)
+        self._state_observer.update(new_state)
 
     def wait_for_states(self, states):
-        return self._state_observer.wait_for (states)
+        return self._state_observer.wait_for(states)
 
     def hello(self):
         self.send('HELLO ' + str(self.id))
-        l.info("sent HELLO")
+        logger.info("sent HELLO")
         self.wait_for_states([SignallingState.HELLO])
 
     def create_session(self, peerid):
         self._peerid = peerid
         self.send('SESSION {}'.format(self._peerid))
-        l.info("sent SESSION")
+        logger.info("sent SESSION")
         self.wait_for_states([SignallingState.SESSION])
 
     def _on_connection(self):
-        self._update_state (SignallingState.OPEN)
+        self._update_state(SignallingState.OPEN)
 
     def _on_message(self, message):
-        l.debug("received: " + message)
+        logger.debug("received: " + message)
         if message == 'HELLO':
-            self._update_state (SignallingState.HELLO)
+            self._update_state(SignallingState.HELLO)
             self.connected.fire()
         elif message == 'SESSION_OK':
-            self._update_state (SignallingState.SESSION)
+            self._update_state(SignallingState.SESSION)
             self.session_created.fire()
         elif message.startswith('ERROR'):
-            self._update_state (SignallingState.ERROR)
+            self._update_state(SignallingState.ERROR)
             self.error.fire(message)
         else:
             msg = json.loads(message)
@@ -194,59 +199,60 @@ class RemoteWebRTCObserver(WebRTCObserver):
     state of a remote peer. Allow performing actions by sending requests over
     the signalling channel.
     """
+
     def __init__(self, signalling):
         super().__init__()
         self.signalling = signalling
 
         def on_json(msg):
             if 'STATE' in msg:
-                state = NegotiationState (msg['STATE'])
+                state = NegotiationState(msg['STATE'])
                 self._update_negotiation_state(state)
                 if state == NegotiationState.OFFER_CREATED:
                     self.on_offer_created.fire(msg['description'])
                 elif state == NegotiationState.ANSWER_CREATED:
                     self.on_answer_created.fire(msg['description'])
                 elif state == NegotiationState.OFFER_SET:
-                    self.on_offer_set.fire (msg['description'])
+                    self.on_offer_set.fire(msg['description'])
                 elif state == NegotiationState.ANSWER_SET:
-                    self.on_answer_set.fire (msg['description'])
+                    self.on_answer_set.fire(msg['description'])
             elif 'DATA-NEW' in msg:
                 new = msg['DATA-NEW']
                 observer = RemoteDataChannelObserver(new['id'], new['location'], self)
-                self.add_channel (observer)
+                self.add_channel(observer)
             elif 'DATA-STATE' in msg:
                 ident = msg['id']
                 channel = self.find_channel(ident)
-                channel._update_state (DataChannelState(msg['DATA-STATE']))
+                channel._update_state(DataChannelState(msg['DATA-STATE']))
             elif 'DATA-MSG' in msg:
                 ident = msg['id']
                 channel = self.find_channel(ident)
                 channel.got_message(msg['DATA-MSG'])
-        self.signalling.have_json.connect (on_json)
+        self.signalling.have_json.connect(on_json)
 
-    def add_data_channel (self, ident):
+    def add_data_channel(self, ident):
         msg = json.dumps({'DATA_CREATE': {'id': ident}})
-        self.signalling.send (msg)
+        self.signalling.send(msg)
 
-    def create_offer (self, ice_restart=False):
-        msg = json.dumps({'CREATE_OFFER': {"ice_restart" : True}})
-        self.signalling.send (msg)
+    def create_offer(self, ice_restart=False):
+        msg = json.dumps({'CREATE_OFFER': {"ice_restart": True}})
+        self.signalling.send(msg)
 
-    def create_answer (self):
+    def create_answer(self):
         msg = json.dumps({'CREATE_ANSWER': ""})
-        self.signalling.send (msg)
+        self.signalling.send(msg)
 
-    def set_title (self, title):
+    def set_title(self, title):
         # entirely for debugging purposes
         msg = json.dumps({'SET_TITLE': title})
-        self.signalling.send (msg)
+        self.signalling.send(msg)
 
-    def set_options (self, opts):
+    def set_options(self, opts):
         options = {}
         if opts.has_field("remote-bundle-policy"):
             options["bundlePolicy"] = opts["remote-bundle-policy"]
-        msg = json.dumps({'OPTIONS' : options})
-        self.signalling.send (msg)
+        msg = json.dumps({'OPTIONS': options})
+        self.signalling.send(msg)
 
 
 class RemoteDataChannelObserver(DataChannelObserver):
@@ -255,14 +261,15 @@ class RemoteDataChannelObserver(DataChannelObserver):
     state of a remote peer's data channel. Allow performing actions by sending
     requests over the signalling channel.
     """
+
     def __init__(self, ident, location, webrtc):
         super().__init__(ident, location)
         self.webrtc = webrtc
 
     def send_string(self, msg):
-        msg = json.dumps({'DATA_SEND_MSG': {'msg' : msg, 'id': self.ident}})
-        self.webrtc.signalling.send (msg)
+        msg = json.dumps({'DATA_SEND_MSG': {'msg': msg, 'id': self.ident}})
+        self.webrtc.signalling.send(msg)
 
-    def close (self):
+    def close(self):
         msg = json.dumps({'DATA_CLOSE': {'id': self.ident}})
-        self.webrtc.signalling.send (msg)
+        self.webrtc.signalling.send(msg)
