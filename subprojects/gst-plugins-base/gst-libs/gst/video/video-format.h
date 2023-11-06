fd8d1edf6cf5471167cef65e1bad528d1968ef30
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2023-11-06 15:16:41 -0500

    video: Add 10bit 422 NV16_10LE40 format
    Similar to NV12_10LE40, this is a 422 variant. This format is also named
    NV20 (20bit per pixels) in other stack and is produced by rkvdec
    decoder.

--- subprojects/gst-plugins-base/gst-libs/gst/video/video-format.h
+++ subprojects/gst-plugins-base/gst-libs/gst/video/video-format.h
@@ -168,6 +168,7 @@ G_BEGIN_DECLS
  * @GST_VIDEO_FORMAT_A420_16BE: planar 4:4:2:0 YUV, 16 bits per channel (Since: 1.24)
  * @GST_VIDEO_FORMAT_GBR_16LE: planar 4:4:4 RGB, 16 bits per channel (Since: 1.24)
  * @GST_VIDEO_FORMAT_GBR_16BE: planar 4:4:4 RGB, 16 bits per channel (Since: 1.24)
+ * @GST_VIDEO_FORMAT_NV16_10LE40: Fully packed variant of NV16_10LE32 (Since: 1.24)
  *
  * Enum value describing the most common video formats.
  *
@@ -609,6 +610,16 @@ typedef enum {
    * Since: 1.24
    */
   GST_VIDEO_FORMAT_GBR_16BE,
+
+
+  /**
+   * GST_VIDEO_FORMAT_NV16_10LE40:
+   *
+   * Fully packed variant of NV16_10LE32
+   *
+   * Since: 1.24
+   */
+  GST_VIDEO_FORMAT_NV16_10LE40,
 } GstVideoFormat;
 
 #define GST_VIDEO_MAX_PLANES 4
@@ -1087,7 +1098,7 @@ gconstpointer  gst_video_format_get_palette          (GstVideoFormat format, gsi
     "AV12, Y444_16BE, GBR_16BE, Y444_16LE, GBR_16LE, v216, P016_BE, P016_LE, " \
     "Y444_12BE, GBR_12BE, Y444_12LE, GBR_12LE, I422_12BE, I422_12LE, Y212_BE, " \
     "Y212_LE, I420_12BE, I420_12LE, P012_BE, P012_LE, Y444_10BE, GBR_10BE, " \
-    "Y444_10LE, GBR_10LE, r210, I422_10BE, I422_10LE, NV16_10LE32, Y210, UYVP, " \
+    "Y444_10LE, GBR_10LE, r210, I422_10BE, I422_10LE, NV16_10LE40, NV16_10LE32, Y210, UYVP, " \
     "v210, I420_10BE, I420_10LE, P010_10BE, MT2110R, MT2110T, NV12_10BE_8L128, " \
     "NV12_10LE40_4L4, P010_10LE, NV12_10LE40, NV12_10LE32, Y444, BGRP, GBR, " \
     "RGBP, NV24, v308, IYU2, RGBx, xRGB, BGRx, xBGR, RGB, BGR, Y42B, NV16, " \
@@ -1106,7 +1117,7 @@ gconstpointer  gst_video_format_get_palette          (GstVideoFormat format, gsi
     "Y444_16LE, GBR_16LE, Y444_16BE, GBR_16BE, v216, P016_LE, P016_BE, " \
     "Y444_12LE, GBR_12LE, Y444_12BE, GBR_12BE, I422_12LE, I422_12BE, Y212_LE, " \
     "Y212_BE, I420_12LE, I420_12BE, P012_LE, P012_BE, Y444_10LE, GBR_10LE, " \
-    "Y444_10BE, GBR_10BE, r210, I422_10LE, I422_10BE, NV16_10LE32, Y210, UYVP, " \
+    "Y444_10BE, GBR_10BE, r210, I422_10LE, I422_10BE, NV16_10LE40, NV16_10LE32, Y210, UYVP, " \
     "v210, I420_10LE, I420_10BE, P010_10LE, NV12_10LE40, NV12_10LE32, " \
     "P010_10BE, MT2110R, MT2110T, NV12_10BE_8L128, NV12_10LE40_4L4, Y444, " \
     "BGRP, GBR, RGBP, NV24, v308, IYU2, RGBx, xRGB, BGRx, xBGR, RGB, BGR, " \
