bbf4849ab85ec61d341c8313178e66e3e49e9f3b
Hsia-Jun(Randy) Li <randy.li@synaptics.com>
2024-02-20 15:48:56 +0800

    v4l2bufferpool: fix enqueue buffer counter
    If the kernel driver returns a valid buffer, it is obviously
    not in the kernel queue anymore.

--- subprojects/gst-plugins-good/sys/v4l2/gstv4l2bufferpool.c
+++ subprojects/gst-plugins-good/sys/v4l2/gstv4l2bufferpool.c
@@ -1280,22 +1280,30 @@ gst_v4l2_buffer_pool_dqbuf (GstV4l2BufferPool * pool, GstBuffer ** buffer,
         group->buffer.index);
   }
 
+  outbuf = pool->buffers[group->buffer.index];
+  if (outbuf == NULL) {
+    if (group->buffer.flags & V4L2_BUF_FLAG_LAST ||
+        group->planes[0].bytesused == 0) {
+      GST_DEBUG_OBJECT (pool, "Last buffer is invalid, signalling eos.");
+      goto eos;
+    }
+
+    goto no_buffer;
+  }
+
+  if (g_atomic_int_dec_and_test (&pool->num_queued)) {
+    GST_OBJECT_LOCK (pool);
+    pool->empty = TRUE;
+    GST_OBJECT_UNLOCK (pool);
+  }
+
   if (group->buffer.flags & V4L2_BUF_FLAG_LAST &&
       group->planes[0].bytesused == 0) {
     GST_DEBUG_OBJECT (pool, "Empty last buffer, signalling eos.");
     goto eos;
   }
 
-  outbuf = pool->buffers[group->buffer.index];
-  if (outbuf == NULL)
-    goto no_buffer;
-
   pool->buffers[group->buffer.index] = NULL;
-  if (g_atomic_int_dec_and_test (&pool->num_queued)) {
-    GST_OBJECT_LOCK (pool);
-    pool->empty = TRUE;
-    GST_OBJECT_UNLOCK (pool);
-  }
 
   timestamp = GST_TIMEVAL_TO_TIME (group->buffer.timestamp);
 
