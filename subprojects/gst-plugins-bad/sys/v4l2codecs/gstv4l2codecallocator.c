e127a82ba95fdfe94b99d369dd70cd37dadc8d56
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2024-05-27 14:28:18 +0200

    v4l2codecs: Add remove buffers helpers
    Add helpers function to call VIDIOC_REMOVE_BUFS ioctl.
    If the driver support this feature buffers are removed from the queue when:
    - the pool when is detached from the decoded.
    - the pool is released.
    - allocation failed.

--- subprojects/gst-plugins-bad/sys/v4l2codecs/gstv4l2codecallocator.c
+++ subprojects/gst-plugins-bad/sys/v4l2codecs/gstv4l2codecallocator.c
@@ -146,6 +146,8 @@ gst_v4l2_codec_allocator_release (GstMiniObject * mini_object)
 {
   GstMemory *mem = GST_MEMORY_CAST (mini_object);
   GstV4l2CodecAllocator *self = GST_V4L2_CODEC_ALLOCATOR (mem->allocator);
+  GstV4l2Decoder *decoder = self->decoder;
+  GstPadDirection direction = self->direction;
   GstV4l2CodecBuffer *buf;
 
   GST_OBJECT_LOCK (self);
@@ -154,9 +156,15 @@ gst_v4l2_codec_allocator_release (GstMiniObject * mini_object)
   gst_memory_ref (mem);
 
   if (gst_v4l2_codec_buffer_release_mem (buf)) {
-    GST_DEBUG_OBJECT (self, "Placing back buffer %i into pool", buf->index);
-    g_queue_push_tail (&self->pool, buf);
-    g_cond_signal (&self->buffer_cond);
+    if (self->detached && gst_v4l2_decoder_has_remove_bufs (decoder)) {
+      GST_DEBUG_OBJECT (self, "Detached pool freeing buffer %d", buf->index);
+      gst_v4l2_decoder_remove_buffers (decoder, direction, buf->index, 1);
+      gst_v4l2_codec_buffer_free (buf);
+    } else {
+      GST_DEBUG_OBJECT (self, "Placing back buffer %i into pool", buf->index);
+      g_queue_push_tail (&self->pool, buf);
+      g_cond_signal (&self->buffer_cond);
+    }
   }
 
   GST_OBJECT_UNLOCK (self);
@@ -173,6 +181,7 @@ gst_v4l2_codec_allocator_prepare (GstV4l2CodecAllocator * self)
 {
   GstV4l2Decoder *decoder = self->decoder;
   GstPadDirection direction = self->direction;
+  GstV4l2CodecBuffer *buf;
   guint i;
 
   GST_DEBUG_OBJECT (self, "Try to create %d buffers", self->pool_size);
@@ -196,7 +205,13 @@ gst_v4l2_codec_allocator_prepare (GstV4l2CodecAllocator * self)
   return TRUE;
 
 failed:
-  gst_v4l2_decoder_request_buffers (decoder, direction, 0);
+  if (gst_v4l2_decoder_has_remove_bufs (decoder)) {
+    while (i-- && (buf = g_queue_pop_tail (&self->pool))) {
+      gst_v4l2_decoder_remove_buffers (decoder, direction, buf->index, 1);
+    }
+  } else {
+    gst_v4l2_decoder_request_buffers (decoder, direction, 0);
+  }
   return FALSE;
 }
 
@@ -210,10 +225,16 @@ static void
 gst_v4l2_codec_allocator_dispose (GObject * object)
 {
   GstV4l2CodecAllocator *self = GST_V4L2_CODEC_ALLOCATOR (object);
+  GstV4l2Decoder *decoder = self->decoder;
+  GstPadDirection direction = self->direction;
   GstV4l2CodecBuffer *buf;
 
-  while ((buf = g_queue_pop_head (&self->pool)))
+  while ((buf = g_queue_pop_head (&self->pool))) {
+    if (gst_v4l2_decoder_has_remove_bufs (decoder)) {
+      gst_v4l2_decoder_remove_buffers (decoder, direction, buf->index, 1);
+    }
     gst_v4l2_codec_buffer_free (buf);
+  }
 
   if (self->decoder) {
     gst_v4l2_codec_allocator_detach (self);
@@ -347,10 +368,14 @@ gst_v4l2_codec_allocator_get_pool_size (GstV4l2CodecAllocator * self)
 void
 gst_v4l2_codec_allocator_detach (GstV4l2CodecAllocator * self)
 {
+  GstV4l2Decoder *decoder = self->decoder;
+
   GST_OBJECT_LOCK (self);
   if (!self->detached) {
     self->detached = TRUE;
-    gst_v4l2_decoder_request_buffers (self->decoder, self->direction, 0);
+    if (!gst_v4l2_decoder_has_remove_bufs (decoder)) {
+      gst_v4l2_decoder_request_buffers (self->decoder, self->direction, 0);
+    }
   }
   GST_OBJECT_UNLOCK (self);
 }
